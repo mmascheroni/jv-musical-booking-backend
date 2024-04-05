5404d43cef8e7e168f4993308637fc4144b49f7c
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-04-05 19:33:07 -0300

    Added the services interfaces of image and product

--- src/main/java/com/musicalbooking/service/IImageService.java
+++ src/main/java/com/musicalbooking/service/IImageService.java
@@ -1,4 +1,19 @@
 package com.musicalbooking.service;
 
+import com.musicalbooking.dto.ImageDto;
+import com.musicalbooking.entity.Image;
+
+import java.util.List;
+
 public interface IImageService {
+
+    ImageDto getImageById(Long id);
+
+    List<ImageDto> getImages();
+
+    ImageDto postImage(Image image);
+
+    ImageDto updateImage(Image image);
+
+    String deleteImageById(Long id);
 }
