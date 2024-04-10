e64f657c97ab6955025882edfedb97bf6d305f99
Mauro Mascheroni <mauromascheronipardo@gmail.com>
2024-04-10 09:52:30 -0300

    Added new method on serviceProduct and controllerProducto for get paging products

--- src/main/java/com/musicalbooking/controller/ProductController.java
+++ src/main/java/com/musicalbooking/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.musicalbooking.exceptions.ResourceNotFoundException;
 import com.musicalbooking.service.impl.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,11 @@ public class ProductController {
     @GetMapping("/{id}")
     public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) throws ResourceNotFoundException {
         return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @GetMapping("/paging")
+    public ResponseEntity<Page<ProductDto>> getProductByPageable(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(productService.getProductsByPageable(page, size));
     }
 
     @PostMapping()
